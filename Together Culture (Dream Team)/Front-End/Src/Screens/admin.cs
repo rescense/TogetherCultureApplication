2910fe84276431bd9b97463750d5804f1bc5096c
Abbas Haider <AH1670@student.aru.ac.uk>
2024-11-14 18:23:50 +0000

    Added admin page

---
+++ Together Culture (Dream Team)/Front-End/Src/Screens/admin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Together_Culture__Dream_Team_.Front_End.Src.Screens
+{
+    public partial class admin : Form
+    {
+        public admin()
+        {
+            InitializeComponent();
+        }
+    }
+}
