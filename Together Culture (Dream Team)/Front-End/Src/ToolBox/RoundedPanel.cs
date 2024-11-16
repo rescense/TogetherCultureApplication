904365f9da055de3eb47ee9aeca9c9cd17c964f5
Aleks <glowacka.aleksandra22@gmail.com>
2024-11-16 20:11:39 +0000

    rounded panel

---
+++ Together Culture (Dream Team)/Front-End/Src/ToolBox/RoundedPanel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Together_Culture__Dream_Team_.Front_End.Src
+{
+    internal class RoundedPanel
+    {
+    }
+}
