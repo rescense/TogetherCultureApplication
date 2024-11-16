1f88ab677ddfc1ea6c92234c5bf14292fa75b6bd
Aleks <glowacka.aleksandra22@gmail.com>
2024-11-16 21:37:01 +0000

    TimeBank to skillshare form

--- Together Culture (Dream Team)/Front-End/Src/Screens/SearchUsersAdmin.cs
+++ Together Culture (Dream Team)/Front-End/Src/Screens/SearchUsersAdmin.cs
@@ -9,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Together_Culture__Dream_Team_.Front_End.Src.ToolBoxItems;
+using Together_Culture__Dream_Team_.Front_End.Src.Screens;
+using togther_Culture;
 
 namespace Together_Culture__Dream_Team_.Front_End.Src.User_Controls
 {
@@ -49,7 +51,13 @@ namespace Together_Culture__Dream_Team_.Front_End.Src.User_Controls
 
         private void timeBankBtn_Click(object sender, EventArgs e)
         {
+            // Create an instance of the skillShareMain form
+            skillShare skillShareForm = new skillShare();
 
+            // Show the skillShareMain form
+            skillShareForm.Show();
+
+            this.Close(); //  completely close the form
         }
     }
 
