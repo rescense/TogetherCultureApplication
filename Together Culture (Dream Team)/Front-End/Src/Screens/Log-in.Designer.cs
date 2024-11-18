231156544f54609de86cef1eefec4126c578f865
Amirhossein Moeini <AM2997@student.aru.ac.uk>
2024-11-18 12:48:19 +0000

    modified the log-in page (front end) back end to be added later

---
+++ Together Culture (Dream Team)/Front-End/Src/Screens/Log-in.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Together_Culture__Dream_Team_.Front_End.Src.Screens
+{
+    partial class Log_in
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Log_in));
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            CheckBox = new CheckBox();
+            guna2CustomGradientPanel1 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            guna2CustomGradientPanel2 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            pictureBox2 = new PictureBox();
+            label7 = new Label();
+            guna2CustomGradientPanel3 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            guna2Button9 = new Guna.UI2.WinForms.Guna2Button();
+            label6 = new Label();
+            label5 = new Label();
+            richTextBox4 = new RichTextBox();
+            richTextBox3 = new RichTextBox();
+            guna2CustomGradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            guna2CustomGradientPanel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // CheckBox
+            // 
+            CheckBox.AutoSize = true;
+            CheckBox.Location = new Point(671, 602);
+            CheckBox.Margin = new Padding(4, 5, 4, 5);
+            CheckBox.Name = "CheckBox";
+            CheckBox.Size = new Size(154, 29);
+            CheckBox.TabIndex = 0;
+            CheckBox.Text = "Remember me";
+            CheckBox.UseVisualStyleBackColor = true;
+            CheckBox.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // guna2CustomGradientPanel1
+            // 
+            guna2CustomGradientPanel1.Controls.Add(guna2CustomGradientPanel2);
+            guna2CustomGradientPanel1.Controls.Add(pictureBox2);
+            guna2CustomGradientPanel1.Controls.Add(label7);
+            guna2CustomGradientPanel1.CustomizableEdges = customizableEdges3;
+            guna2CustomGradientPanel1.Dock = DockStyle.Top;
+            guna2CustomGradientPanel1.FillColor = Color.DarkRed;
+            guna2CustomGradientPanel1.FillColor2 = Color.LightCoral;
+            guna2CustomGradientPanel1.FillColor3 = Color.Maroon;
+            guna2CustomGradientPanel1.FillColor4 = Color.Firebrick;
+            guna2CustomGradientPanel1.Location = new Point(0, 0);
+            guna2CustomGradientPanel1.Margin = new Padding(4, 5, 4, 5);
+            guna2CustomGradientPanel1.Name = "guna2CustomGradientPanel1";
+            guna2CustomGradientPanel1.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            guna2CustomGradientPanel1.Size = new Size(2057, 300);
+            guna2CustomGradientPanel1.TabIndex = 1;
+            // 
+            // guna2CustomGradientPanel2
+            // 
+            guna2CustomGradientPanel2.CustomizableEdges = customizableEdges1;
+            guna2CustomGradientPanel2.FillColor = Color.DarkRed;
+            guna2CustomGradientPanel2.FillColor2 = Color.LightCoral;
+            guna2CustomGradientPanel2.Location = new Point(0, 298);
+            guna2CustomGradientPanel2.Name = "guna2CustomGradientPanel2";
+            guna2CustomGradientPanel2.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2CustomGradientPanel2.Size = new Size(2057, 1025);
+            guna2CustomGradientPanel2.TabIndex = 2;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(1086, 81);
+            pictureBox2.Margin = new Padding(4, 5, 4, 5);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(146, 153);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 18;
+            pictureBox2.TabStop = false;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.Font = new Font("Arial Rounded MT Bold", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.ForeColor = SystemColors.ButtonHighlight;
+            label7.Location = new Point(748, 39);
+            label7.Margin = new Padding(4, 0, 4, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(330, 195);
+            label7.TabIndex = 17;
+            label7.Text = "Together\r\nCulture\r\nCambridge";
+            // 
+            // guna2CustomGradientPanel3
+            // 
+            guna2CustomGradientPanel3.Controls.Add(guna2Button9);
+            guna2CustomGradientPanel3.Controls.Add(label6);
+            guna2CustomGradientPanel3.Controls.Add(label5);
+            guna2CustomGradientPanel3.Controls.Add(CheckBox);
+            guna2CustomGradientPanel3.Controls.Add(richTextBox4);
+            guna2CustomGradientPanel3.Controls.Add(richTextBox3);
+            guna2CustomGradientPanel3.CustomizableEdges = customizableEdges7;
+            guna2CustomGradientPanel3.FillColor = Color.DarkRed;
+            guna2CustomGradientPanel3.FillColor2 = Color.LightCoral;
+            guna2CustomGradientPanel3.Location = new Point(-59, 298);
+            guna2CustomGradientPanel3.Name = "guna2CustomGradientPanel3";
+            guna2CustomGradientPanel3.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            guna2CustomGradientPanel3.Size = new Size(2116, 1116);
+            guna2CustomGradientPanel3.TabIndex = 2;
+            // 
+            // guna2Button9
+            // 
+            guna2Button9.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            guna2Button9.AutoRoundedCorners = true;
+            guna2Button9.BackColor = Color.Transparent;
+            guna2Button9.BorderRadius = 58;
+            guna2Button9.CustomizableEdges = customizableEdges5;
+            guna2Button9.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button9.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button9.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button9.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button9.FillColor = Color.Black;
+            guna2Button9.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            guna2Button9.ForeColor = Color.White;
+            guna2Button9.Location = new Point(920, 690);
+            guna2Button9.Margin = new Padding(4, 5, 4, 5);
+            guna2Button9.Name = "guna2Button9";
+            guna2Button9.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            guna2Button9.Size = new Size(306, 118);
+            guna2Button9.TabIndex = 25;
+            guna2Button9.Text = "Login";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = SystemColors.ButtonFace;
+            label6.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Bold);
+            label6.Location = new Point(671, 462);
+            label6.Name = "label6";
+            label6.Size = new Size(204, 46);
+            label6.TabIndex = 24;
+            label6.Text = "Password";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = SystemColors.ButtonFace;
+            label5.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Bold);
+            label5.Location = new Point(671, 255);
+            label5.Name = "label5";
+            label5.Size = new Size(137, 46);
+            label5.TabIndex = 23;
+            label5.Text = "Email ";
+            // 
+            // richTextBox4
+            // 
+            richTextBox4.Location = new Point(671, 526);
+            richTextBox4.Name = "richTextBox4";
+            richTextBox4.Size = new Size(812, 68);
+            richTextBox4.TabIndex = 22;
+            richTextBox4.Text = "";
+            // 
+            // richTextBox3
+            // 
+            richTextBox3.Location = new Point(671, 321);
+            richTextBox3.Name = "richTextBox3";
+            richTextBox3.Size = new Size(812, 68);
+            richTextBox3.TabIndex = 21;
+            richTextBox3.Text = "";
+            richTextBox3.TextChanged += richTextBox3_TextChanged;
+            // 
+            // Log_in
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(2057, 1415);
+            Controls.Add(guna2CustomGradientPanel3);
+            Controls.Add(guna2CustomGradientPanel1);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "Log_in";
+            Text = "Log_in";
+            Load += Log_in_Load;
+            guna2CustomGradientPanel1.ResumeLayout(false);
+            guna2CustomGradientPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            guna2CustomGradientPanel3.ResumeLayout(false);
+            guna2CustomGradientPanel3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private CheckBox CheckBox;
+        private Guna.UI2.WinForms.Guna2CustomGradientPanel guna2CustomGradientPanel1;
+        private PictureBox pictureBox2;
+        private Label label7;
+        private Guna.UI2.WinForms.Guna2CustomGradientPanel guna2CustomGradientPanel2;
+        private Guna.UI2.WinForms.Guna2CustomGradientPanel guna2CustomGradientPanel3;
+        private Label label6;
+        private Label label5;
+        private RichTextBox richTextBox4;
+        private RichTextBox richTextBox3;
+        private Guna.UI2.WinForms.Guna2Button guna2Button9;
+    }
+}
